3bb82eeab38b2df5e995a37cfd2101dd3d8db1fd
Ubuntu <ubuntu@ip-172-31-34-191.ap-northeast-1.compute.internal>
2020-04-23 18:40:19 +0000

    create robot

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include, re_path
 from posts.views import PostListView, emailview, successview
+from django.views.generic.base import TemplateView
 
 from posts.models import Post
 from . import views
@@ -12,7 +13,9 @@ urlpatterns = [
     re_path(r'(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
     path('contact/', views.emailview, name='email'),
     path('success/', views.successview, name='success'),
-path('search-result/', views.searchlistview, name='search-result'),
+    path('search-result/', views.searchlistview, name='search-result'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt',content_type="text/plain")),
+    #  {'template': 'robots.txt', 'mimetype': 'text/plain'}),
     path('', views.top, name='top'),
     # path('contact/', views.contact, name='contact'),
 ]
